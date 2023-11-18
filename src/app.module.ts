0464ac91d041393ce998c56e9e36b4e69c3e43f9
Silva_jr <josedasilva0112@gmail.com>
2023-11-18 12:08:33 +0100

    update

--- src/app.module.ts
+++ src/app.module.ts
@@ -4,9 +4,8 @@ import { AppService } from './app.service';
 import { ScheduleModule } from './schedule/schedule.module';
 import { UsersModule } from './users/users.module';
 import { MongooseModule } from '@nestjs/mongoose';
-import { JWT_SECRET, MONGO_CONNECT, jwtConstants } from './constants';
+import { MONGO_CONNECT } from './constants';
 import { AuthModule } from './auth/auth.module';
-import { JwtModule } from '@nestjs/jwt';
 
 @Module({
   imports: [
